f2576c6f8bce61c28e61a16767a5e5826e47a56a
gRoussac <gregory.roussac@prestashop.com>
2014-05-02 23:16:33 +0200

    [*] New module version 1.5.3

--- blockbestsellers.php
+++ blockbestsellers.php
@@ -35,7 +35,7 @@ class BlockBestSellers extends Module
 	{
 		$this->name = 'blockbestsellers';
 		$this->tab = 'front_office_features';
-		$this->version = '1.5.2';
+		$this->version = '1.5.3';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 		$this->bootstrap = true;
