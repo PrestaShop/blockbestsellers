f956674fcb02e36e86ca74380940a4d027b8665a
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-08-18 11:14:21 +0200

    //Version update

--- blockbestsellers.php
+++ blockbestsellers.php
@@ -35,7 +35,7 @@ class BlockBestSellers extends Module
 	{
 		$this->name = 'blockbestsellers';
 		$this->tab = 'front_office_features';
-		$this->version = '1.5.5';
+		$this->version = '1.5.6';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 		$this->bootstrap = true;
